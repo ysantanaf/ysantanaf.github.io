94191e4ed8375cbae95872636c835963f3faf5f5
Imanol QB <imanolquinber26@gmail.com>
2025-07-25 15:46:17 +0100

    Eliminar directorios ignorados del seguimiento de Git

--- node_modules/@ionic/core/components/ion-content.js
+++
@@ -1,9 +0,0 @@
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-import { C as Content, d as defineCustomElement$1 } from './content.js';
-
-const IonContent = Content;
-const defineCustomElement = defineCustomElement$1;
-
-export { IonContent, defineCustomElement };
